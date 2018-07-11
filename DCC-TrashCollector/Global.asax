75cae843ad601324526b0f1971958806b1d25553
parishwebdev <parishwebdev@gmail.com>
2018-07-10 20:48:56 -0500

    added initial structure

---
+++ DCC-TrashCollector/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DCC_TrashCollector.MvcApplication" Language="C#" %>
---
+++ DCC-TrashCollector/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DCC_TrashCollector.Startup))]
+namespace DCC_TrashCollector
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
